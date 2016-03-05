7ee6ea59d2a8d94f9fbf724a9da394a39c645503
kiselev1189 <kiselev1189@gmail.com>
2016-03-05 16:00:48 +0300

    Now yielding vector - label pairs

--- vectorizer.py
+++ vectorizer.py
@@ -1,18 +1,18 @@
 import json
 import numpy as np
-from text_density import text_density
 from stop_words import get_stop_words
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from batcher import batch
+from keras.utils import np_utils
 import config
 
 
 def bag_of_words_vectorizer(datafile, k_features):
     """
     Computes sparse term-document matrix of datafile documents, selects k best features by chi2 test.
-    Yields batches of BATCH_SIZE of dense tdm vectors.
+    Yields batches of BATCH_SIZE of dense tdm vectors and vector of labels, transformed for keras nn.
     """
     data = []
     labels = []
@@ -27,6 +27,16 @@ def bag_of_words_vectorizer(datafile, k_features):
     data = vectorizer.fit_transform(data)
     data = SelectKBest(chi2, k=k_features).fit_transform(data, labels)
 
-    for vector_batch in batch(data, config.BATCH_SIZE):
-        yield np.vstack([i.toarray() for i in vector_batch])
+    for vector_label_batch in batch(zip(data, labels), config.BATCH_SIZE):
+        vectors = []
+        labels = []
+        for vec_label in vector_label_batch:
+            vectors.append(vec_label[0].toarray())
+            labels.append(vec_label[1])
+
+        X = np.vstack(vectors)
+        Y = np_utils.to_categorical(labels, 2)
+        yield X, Y
+
+
 
