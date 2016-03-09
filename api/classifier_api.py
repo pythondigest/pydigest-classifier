4186f9ec4350ba2d66cae84a4d870ca052bb1ba3
kiselev1189 <kiselev1189@gmail.com>
2016-03-10 01:17:03 +0300

    Simple API

---
+++ api/classifier_api.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import json
+from sklearn.externals import joblib
+
+app = Flask(__name__)
+
+classifier = joblib.load("classifier_dump.pkl")
+
+
+@app.route("/api/v1.0/classify/", methods=["POST"])
+def classify():
+    json_data = request.get_json(force=True)
+
+    links = []
+    for entity in json_data:
+        links.append(entity["link"])
+
+    predicted = classifier.predict(json_data)
+
+    out = [{i[0]: bool(i[1])} for i in zip(links, predicted)]
+    print(out)
+    return json.dumps(out)
+
+if __name__ == '__main__':
+    app.run(debug=True)
