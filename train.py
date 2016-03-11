d6dc2f6f7d1d6645435557b64e80ab8677b42933
kiselev1189 <kiselev1189@gmail.com>
2016-03-11 18:51:07 +0300

    Trainer

--- train.py
+++ train.py
@@ -1,21 +1,37 @@
-from sklearn.pipeline import Pipeline
+import sys
+import os
 import json
+from sklearn.pipeline import Pipeline
 from sklearn.externals import joblib
 
 from vectorizer import GeomFeatureExtractor
 from classifier import ChainedClassifier
 
-input_data = []
-input_labels = []
 
-raw_docs = json.loads(open("data_final.json").read())
-for i in raw_docs:
-    input_data.append(i)
-    input_labels.append(i["label"])
+"""
+Trains and dumps the classifier.
+Accepts 2 args: first one is input path, second one is output path
+Classifier is trained on all files with .json extension from input path
+Classifier is saved to the file specified by output path
+"""
 
-text_clf = Pipeline([('vect', GeomFeatureExtractor()),
-                     ('clf', ChainedClassifier())
-                    ])
 
-text_clf.fit(input_data, input_labels)
-joblib.dump(text_clf, "classifier_dump.pkl", compress=1)
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+    out_path = sys.argv[2]
+    input_data = []
+    input_labels = []
+
+    for file in os.listdir(input_path):
+        if file.endswith(".json"):
+            raw_docs = json.loads(open(file, errors="ignore").read())
+            for i in raw_docs:
+                input_data.append(i)
+                input_labels.append(i["label"])
+
+    text_clf = Pipeline([('vect', GeomFeatureExtractor()),
+                        ('clf', ChainedClassifier())
+                        ])
+
+    text_clf.fit(input_data, input_labels)
+    joblib.dump(text_clf, out_path, compress=1)
